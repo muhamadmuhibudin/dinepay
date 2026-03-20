a584e8655071ecd4ca6d40e618a5c539b1c0e757
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-03-20 16:03:21 +0700

    feat(customer-layouts): add footer partial

---
+++ resources/views/customer/layouts/__footer.blade.php
@@ -0,0 +1,67 @@
+<!-- Footer Start -->
+<div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5">
+    <div class="container py-5">
+        <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(226, 175, 24, 0.5);">
+            <div class="row g-4">
+                <div class="col-lg-6">
+                    <a href="{{ route('home') }}">
+                        <h1 class="text-primary mb-0">DinePay</h1>
+                        <p class="text-secondary mb-0">Delicious choices at your fingertips!</p>
+                    </a>
+                </div>
+                <div class="col-lg-6">
+                    <div class="d-flex justify-content-end pt-3">
+                        <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href="https://www.instagram.com/yourbrand"><i class="fab fa-instagram"></i></a>
+                        <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href="https://tiktok.com/@yourbrand"><i class="fab fa-tiktok"></i></a>
+                        <a class="btn btn-outline-secondary me-2 btn-md-square rounded-circle" href="https://www.youtube.com/@yourbrand"><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-secondary btn-md-square rounded-circle" href="https://linkedin.com/company/yourbrand"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row g-5">
+            <div class="col-lg-4 col-md-6">
+                <div class="footer-item">
+                    <h4 class="text-light mb-3">Why Choose Us</h4>
+                    <p class="mb-4">We provide convenience, the best menu options, and fast service to make your dining experience more enjoyable!</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="footer-item">
+                    <h4 class="text-light mb-3">Contact Us</h4>
+                    <p>Address: Jl. Sangkuriang No.8, Cimahi Tengah, Cimahi City, West Java 40526</p>
+                    <p>Email: info@dinepay.com</p>
+                    <p>Phone: +62 899-9488-990</p>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="d-flex flex-column text-start footer-item">
+                    <h4 class="text-light mb-3">Payment Methods</h4>
+                    <a class="btn-link" href="#">QR</a>
+                    <a class="btn-link" href="#">Cash</a>
+                    <a class="btn-link" href="#">Stripe</a>
+                    <a class="btn-link" href="#">PayPal</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
+
+<!-- Copyright Start -->
+<div class="container-fluid copyright bg-dark py-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                <span class="text-light">
+                    <a href="{{ route('home') }}"><i class="fas fa-copyright text-light me-2"></i>DinePay</a>
+                    <span id="currentYear"></span>. All rights reserved.
+                </span>
+            </div>
+            <div class="col-md-6 my-auto text-center text-md-end text-white">
+                Designed By <a class="border-bottom" href="https://htmlcodex.com">HTML Codex</a> Distributed By <a class="border-bottom" href="#">Muhammad Muhibbudin</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Copyright End -->
