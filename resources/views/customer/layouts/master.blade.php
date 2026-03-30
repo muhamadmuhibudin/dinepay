e6f1e6bdaca056b556a6d3cf02bcc9a57b98e98b
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-03-30 09:45:03 +0700

    chore(customer-layouts): setup master layout

---
+++ resources/views/customer/layouts/master.blade.php
@@ -0,0 +1,32 @@
+@include('customer.layouts.__header')
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" role="status"></div>
+    </div>
+    <!-- Spinner End -->
+
+    @include('customer.layouts.__navbar')
+
+    <!-- Single Page Header start -->
+    <div class="container-fluid page-header py-5">
+        <h1 class="text-center text-white display-6">Our Menu</h1>
+        <ol class="breadcrumb justify-content-center mb-0">
+            <li class="breadcrumb-item active text-primary">Please choose your favorite menu</li>
+        </ol>
+    </div>
+    <!-- Single Page Header End -->
+
+    @yield('content')
+
+    @include('customer.layouts.__footer')
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-primary border-3 border-primary rounded-circle back-to-top"><i class="fa fa-arrow-up"></i></a>
+
+    <script>
+        document.getElementById('currentYear').textContent = new Date().getFullYear();
+    </script>
+</body>
+</html>
