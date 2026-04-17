61eb4766fe2826f7ac4c0d690a63e4550a628222
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-04-17 20:53:50 +0700

    fix(customer-js): refactor app.js to resolve loading issue

--- resources/assets/customer/js/app.js
+++ resources/assets/customer/js/app.js
@@ -1,10 +1,8 @@
 import "./bootstrap";
 import "./vendor";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "../lib/easing/easing.min.js";
-import "../lib/waypoints/waypoints.min.js";
 
-// Ensure jQuery global is initialized before loading jQuery plugins.
-await import("../lib/lightbox/js/lightbox.min.js");
-await import("../lib/owlcarousel/owl.carousel.min.js");
-await import("./main");
+// Import main functionality
+import "./main";
+
+console.log('App loaded successfully');
