fee752cd594cda287c9318eaec7243d9617f2b9d
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-04-17 20:54:04 +0700

    fix(customer-js): adjust main.js logic to ensure content renders

--- resources/assets/customer/js/main.js
+++ resources/assets/customer/js/main.js
@@ -3,15 +3,25 @@ import $ from 'jquery';
 (function ($) {
     'use strict';
 
+    console.log('Main.js loaded');
+
     // Spinner
     var spinner = function () {
+        console.log('Spinner function called');
         setTimeout(function () {
+            console.log('Removing spinner');
             if ($('#spinner').length > 0) {
                 $('#spinner').removeClass('show');
+                console.log('Spinner removed');
             }
-        }, 1);
+        }, 1000);
     };
-    spinner(0);
+    
+    // Wait for DOM
+    $(document).ready(function() {
+        console.log('DOM ready');
+        spinner();
+    });
 
     // Fixed Navbar
     $(window).scroll(function () {
@@ -127,4 +137,47 @@ import $ from 'jquery';
 
         button.parent().parent().find('input').val(newVal);
     });
+
+    // Add to Cart functionality
+    $('.btn').on('click', function(e) {
+        if ($(this).text().includes('Add to Cart')) {
+            e.preventDefault();
+            
+            var button = $(this);
+            var originalText = button.html();
+            
+            // Show loading state
+            button.html('<i class="fa fa-spinner fa-spin me-2"></i> Adding...');
+            button.prop('disabled', true);
+            
+            // Simulate adding to cart
+            setTimeout(function() {
+                button.html('<i class="fa fa-check me-2 text-success"></i> Added!');
+                button.removeClass('text-primary').addClass('text-success');
+                
+                // Update cart count
+                updateCartCount();
+                
+                // Reset button after 2 seconds
+                setTimeout(function() {
+                    button.html(originalText);
+                    button.removeClass('text-success').addClass('text-primary');
+                    button.prop('disabled', false);
+                }, 2000);
+            }, 500);
+        }
+    });
+
+    // Update cart count function
+    function updateCartCount() {
+        var cartIcon = $('.fa-shopping-bag').parent();
+        var badge = cartIcon.find('.badge');
+        
+        if (badge.length === 0) {
+            cartIcon.append('<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">1</span>');
+        } else {
+            var count = parseInt(badge.text());
+            badge.text(count + 1);
+        }
+    }
 })($);
