95481d0ac66a26b11e64fa0a54ca85ef1d2fac27
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-04-25 20:52:31 +0700

    fix(migrations): update users table migration to include remember_token column for persistent login support

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('phone');
             $table->unsignedBigInteger('role_id');
+            $table->rememberToken();
             $table->softDeletes();
             $table->timestamps();
 
