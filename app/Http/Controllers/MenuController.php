a60ed00496f9356dceea046a7f5a4dff5e761169
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-04-26 21:02:53 +0700

    feat(controller): extend MenuController with cart and addToCart methods to manage session-based shopping cart

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -20,4 +20,42 @@ class MenuController extends Controller
 
         return view('customer.menu', compact('tableNumber', 'items'));
     }
+
+    public function cart()
+    {
+        $cart = session::get('cart', []);
+        return view('customer.cart', compact('cart'));
+    }
+
+    public function addToCart(Request $request)
+    {
+        $menuId = $request->input('id');
+        $menu = Item::find($menuId);
+
+        if (!$menu) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Menu not found'
+            ], 404);
+        }
+
+        $cart = session::get('cart', []);
+        if (isset($cart[$menuId])) {
+            $cart[$menuId]['qty'] += 1;
+        } else {
+            $cart[$menuId] = [
+                'id' => $menu->id,
+                'name' => $menu->name,
+                'price' => $menu->price,
+                'image' => $menu->img,
+                'qty' => 1
+            ];
+        }
+        session::put('cart', $cart);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Item added to cart',
+            'cart' => $cart
+        ]);
+    }
 }
