db950db5e784d91db397f3eb48802aecff9bfea6
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-04-30 20:30:13 +0700

    fix(views): update cart.blade to loop session cart items with array syntax and remove dummy row

--- resources/views/customer/cart.blade.php
+++ resources/views/customer/cart.blade.php
@@ -10,112 +10,75 @@
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
         @endif
-        @if(!empty($cart))
-        <h4 class="text-center">Your cart is empty !</h4>
-        @else
-         <div class="table-responsive">
-            <table class="table">
-                <thead>
-                  <tr>
-                    <th scope="col">Image</th>
-                    <th scope="col">Item</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Quantity</th>
-                    <th scope="col">Total</th>
-                    <th scope="col">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    @php $subtotal = 0; @endphp
-                    @forelse($cart as $item)
-                    @php
-                        $itemTotal = $item('price') * $item('quantity');
-                        $subtotal += $itemTotal;
-                    @endphp
 
-                    <tr>
-                        <td>{{ $item['name'] }}</td>
-                        <td>Rp{{ number_format($item['price'], 0, ',', '.') }}</td>
-                        <td>{{ $item['qty'] }}</td>
-                        <td>Rp{{ number_format($itemTotal, 0, ',', '.') }}</td>
-                    </tr>
-                    
-                    <tr>
-                        <th scope="row">
-                            <div class="d-flex align-items-center">
-                                <img src="https://images.unsplash.com/photo-1591325418441-ff678baf78ef" class="img-fluid me-5 rounded-circle" style="width: 80px; height: 80px;" alt="">
+        @if(empty($cart))
+            <h4 class="text-center">Your cart is empty!</h4>
+        @else
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Image</th>
+                            <th scope="col">Item</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Total</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php $subtotal = 0; @endphp
+                        @foreach($cart as $item)
+                            @php
+                                $itemTotal = $item['price'] * $item['qty'];
+                                $subtotal += $itemTotal;
+                            @endphp
+                            <tr>
+                                <td>
+                                    <img src="{{ $item['image'] }}" class="img-fluid rounded-circle" style="width:80px; height:80px;" alt="{{ $item['name'] }}">
+                                </td>
+                                <td>{{ $item['name'] }}</td>
+                                <td>Rp{{ number_format($item['price'], 0, ',', '.') }}</td>
+                                <td>{{ $item['qty'] }}</td>
+                                <td>Rp{{ number_format($itemTotal, 0, ',', '.') }}</td>
+                                <td>
+                                    <button class="btn btn-sm btn-danger">Remove</button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="row g-4 justify-content-end mt-1">
+                <div class="col-8"></div>
+                <div class="col-sm-8 col-md-7 col-lg-6 col-xl-4">
+                    <div class="bg-light rounded">
+                        <div class="p-4">
+                            <h2 class="display-6 mb-4">Order <span class="fw-normal">Summary</span></h2>
+                            <div class="d-flex justify-content-between mb-4">
+                                <h5 class="mb-0 me-4">Subtotal</h5>
+                                <p class="mb-0">Rp{{ number_format($subtotal, 0, ',', '.') }}</p>
                             </div>
-                        </th>
-                        <td>
-                            <p class="mb-0 mt-4">Ichiraku Ramen</p>
-                        </td>
-                        <td>
-                            <p class="mb-0 mt-4">Rp25,000.00</p>
-                        </td>
-                        <td>
-                            <div class="input-group quantity mt-4" style="width: 100px;">
-                                <div class="input-group-btn">
-                                    <button class="btn btn-sm btn-minus rounded-circle bg-light border">
-                                        <i class="fa fa-minus"></i>
-                                    </button>
-                                </div>
-                                <input type="text" class="form-control form-control-sm text-center border-0" value="1">
-                                <div class="input-group-btn">
-                                    <button class="btn btn-sm btn-plus rounded-circle bg-light border">
-                                        <i class="fa fa-plus"></i>
-                                    </button>
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <p class="mb-0 mt-4">Rp25,000.00</p>
-                        </td>
-                        <td>
-                            <button class="btn btn-md rounded-circle bg-light border mt-4">
-                                <i class="fa fa-times text-danger"></i>
-                            </button>
-                        </td>
-                    </tr>
-                    @empty
-                    <tr>
-                        <td colspan="6" class="text-center">Cart is empty</td>
-                    </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
-        <div class="row g-4 justify-content-end mt-1">
-            <div class="col-8"></div>
-            <div class="col-sm-8 col-md-7 col-lg-6 col-xl-4">
-                <div class="bg-light rounded">
-                    <div class="p-4">
-                        <h2 class="display-6 mb-4">Order <span class="fw-normal">Summary</span></h2>
-                        <div class="d-flex justify-content-between mb-4">
-                            <h5 class="mb-0 me-4">Subtotal</h5>
-                            <p class="mb-0">Rp85,000.00</p>
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <p class="mb-0 me-4">Tax (10%)</p>
-                            <div class="">
-                                <p class="mb-0">Rp8,500.00</p>
+                            <div class="d-flex justify-content-between">
+                                <p class="mb-0 me-4">Tax (10%)</p>
+                                <p class="mb-0">Rp{{ number_format($subtotal * 0.1, 0, ',', '.') }}</p>
                             </div>
                         </div>
+                        <div class="py-4 mb-4 border-top d-flex justify-content-between">
+                            <h4 class="mb-0 ps-4 me-4">Total</h4>
+                            <h5 class="mb-0 pe-4">Rp{{ number_format($subtotal * 1.1, 0, ',', '.') }}</h5>
+                        </div>
                     </div>
-                    <div class="py-4 mb-4 border-top d-flex justify-content-between">
-                        <h4 class="mb-0 ps-4 me-4">Total</h4>
-                        <h5 class="mb-0 pe-4">Rp93,500.00</h5>
-                    </div>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <div class="mb-0 mb-3">
-                        <a href="{{ route('checkout') }}" class="btn border-secondary py-3 text-primary text-uppercase mb-4" type="button">Proceed to Checkout</a>
+                    <div class="d-flex justify-content-end">
+                        <div class="mb-0 mb-3">
+                            <a href="{{ route('checkout') }}" class="btn border-secondary py-3 text-primary text-uppercase mb-4" type="button">Proceed to Checkout</a>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         @endif
     </div>
 </div>
 <!-- Cart Page End -->
-
 @endsection
