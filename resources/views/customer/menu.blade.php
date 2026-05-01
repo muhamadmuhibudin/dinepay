be7c86574fd49cb8db94e5370368601db99ca69d
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-05-01 14:25:11 +0700

    feat(views): add proper Add to Cart button with item ID and fix fetch syntax for cart.add route

--- resources/views/customer/menu.blade.php
+++ resources/views/customer/menu.blade.php
@@ -38,22 +38,7 @@
                                     </div>
 
                                 @endforeach
-
-                                    
-
-                                    <!-- Pagination -->
-                                    <!-- <div class="col-12">
-                                        <div class="pagination d-flex justify-content-center mt-5">
-                                            <a href="#" class="rounded">&laquo;</a>
-                                            <a href="#" class="active rounded">1</a>
-                                            <a href="#" class="rounded">2</a>
-                                            <a href="#" class="rounded">3</a>
-                                            <a href="#" class="rounded">4</a>
-                                            <a href="#" class="rounded">5</a>
-                                            <a href="#" class="rounded">6</a>
-                                            <a href="#" class="rounded">&raquo;</a>
-                                        </div>
-                                    </div> -->
+                                
                                 </div>
                             </div>
                         </div>
@@ -63,22 +48,23 @@
         </div>
         <!-- Fruits Shop End-->
          <script>
-            function addToCart(menuId) {
-                fetch "{{ route('cart.add') }}", {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-CSRF-TOKEN': '{{ csrf_token() }}'
-                    },
-                    body: JSON.stringify({id: menuId})
-                }
-                .then(response => response.json())
-                .then(data => {
-                    alert(data.message);
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-            }
-         </script>
+    function addToCart(menuId) {
+        fetch("{{ route('cart.add') }}", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "X-CSRF-TOKEN": "{{ csrf_token() }}"
+            },
+            body: JSON.stringify({ id: menuId })
+        })
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+    }
+</script>
+
 @endsection
