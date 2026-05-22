17772d17244e9ef0b711fff1eb81ace331c88883
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-05-22 15:34:12 +0700

    test(routes): update feature tests to reflect revised customer entry flow

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,16 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
+    public function test_home_redirects_to_menu_with_default_table(): void
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect(route('menu.index', ['table' => 1]));
+    }
+
+    public function test_removed_customer_placeholder_routes_return_not_found(): void
+    {
+        $this->get('/contact')->assertNotFound();
+        $this->get('/customer/welcome')->assertNotFound();
     }
 }
