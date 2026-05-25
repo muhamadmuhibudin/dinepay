5d90f88863a5514f7eeb96f5c2e7dfae4ef4ed46
Muhammad Muhibbudin <muh.muhibudin@gmail.com>
2026-05-25 16:35:30 +0700

    build(vite): adjust Vite configuration for Laravel frontend workflow

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,15 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
-                'resources/js/app.js',
                 'resources/assets/customer/css/app.css',
-                'resources/assets/customer/js/app.js'
+                'resources/assets/customer/js/app.js',
             ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
     server: {
         watch: {
